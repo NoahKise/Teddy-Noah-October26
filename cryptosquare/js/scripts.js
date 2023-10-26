40de3bcb3cae9a7e1e92746b9a46fa25dab9ad9f
noahkise <noah@kisefamily.com>
2023-10-26 15:57:26 -0700

    WIP
    Co-authored-by Teddy Peterschmidt <t.a.peterschmidt@gmail.com>:

--- cryptosquare/js/scripts.js
+++ cryptosquare/js/scripts.js
@@ -1,16 +1,26 @@
-function removingSpacesAndPunctuation(input) {
+function crypto(input) {
     const lettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     const editedArray = [];
     const inputArray = input.toLowerCase().split("")
     inputArray.forEach(function(element) {
         if (lettersArray.includes(element)) {
             editedArray.push(element);
-            console.log(editedArray);
         }
     })
     const squareRoot = Math.sqrt(editedArray.length);
     const roundedRoot = Math.floor(squareRoot);
-    console.log(roundedRoot);
+    let joinedString = editedArray.join("")
+    let splitArray = [];
+    let lastArray = [];
+    let splitString = []
+    for (let i = 0; i < joinedString.length; i +=roundedRoot) {
+        splitString.push((joinedString.slice(i, i + roundedRoot)));
+        // splitArray.push(splitString);
+    }
+    for (let i = 0; i < roundedRoot; i +=1) {
+        lastArray.push(splitString[i]);
+        console.log(lastArray);
+    }
 }
 
 // function splitArrayBySquareRoot(input) {
