2448e27b393fd7a356735eb9d22459e714f6fa8e
noahkise <noah@kisefamily.com>
2023-10-26 11:08:47 -0700

    pass thousands place test
    Co-authored-by Teddy Peterschmidt <t.a.peterschmidt@gmail.com>:

--- roman-numerals/README.md
+++ roman-numerals/README.md
@@ -14,6 +14,10 @@ Test: It will transform input to an array of numbers.
 Code roman("267")
 Result: [2, 6, 7]
 
-Test: If string lenght is 2, return roman numeral.
+Test: If string length is 2, return roman numeral for tens place.
 Code: roman(20)
 return: XX
+
+Test: If string length is 3, return roman numeral for hundreds place.
+Code: roman(300);
+Return: CCC
--- roman-numerals/js/scripts.js
+++ roman-numerals/js/scripts.js
@@ -2,15 +2,13 @@
 // const inputArray = inputNum.split("");
 
 function onesPlace(input) {
-    const inputNum = parseInt(input);
     const inputArray = input.split("");
     const numArray = inputArray.map(function (num) {
         return parseInt(num);
     })
     console.log(numArray);
-
     if (numArray[numArray.length - 1] <= 3) {
-        return "I".repeat(inputNum);
+        return "I".repeat(numArray[numArray.length - 1]);
     } else if (numArray[numArray.length - 1] === 4) {
         return "IV";
     } else if (numArray[numArray.length - 1] === 5) {
@@ -23,5 +21,47 @@ function onesPlace(input) {
 }
 
 function tensPlace(input) {
-    if (inputNum)
+    const inputArray = input.split("");
+    const numArray = inputArray.map(function (num) {
+        return parseInt(num);
+    })
+    if (numArray[numArray.length - 2] <= 3) {
+        return "X".repeat(numArray[numArray.length - 2]);
+    } else if (numArray[numArray.length - 2] === 4) {
+        return "XL";
+    } else if (numArray[numArray.length - 2] === 5) {
+        return "L";
+    } else if (numArray[numArray.length - 2] > 5 && numArray[numArray.length - 2] < 9) {
+        return "L" + "X".repeat(numArray[numArray.length - 2] - 5);
+    } else {
+        return "XC";
+    }
+}
+
+function hundredsPlace(input) {
+    const inputArray = input.split("");
+    const numArray = inputArray.map(function (num) {
+        return parseInt(num);
+    })
+    if (numArray[numArray.length - 3] <= 3) {
+        return "C".repeat(numArray[numArray.length - 3]);
+    } else if (numArray[numArray.length - 3] === 4) {
+        return "DC";
+    } else if (numArray[numArray.length - 3] === 5) {
+        return "D";
+    } else if (numArray[numArray.length - 3] > 5 && numArray[numArray.length - 3] < 9) {
+        return "D" + "C".repeat(numArray[numArray.length - 3] - 5);
+    } else {
+        return "CM";
+    }
+}
+
+function thousandsPlace(input) {
+    const inputArray = input.split("");
+    const numArray = inputArray.map(function (num) {
+        return parseInt(num);
+    })
+    if (numArray[numArray.length - 4] <= 3) {
+        return "M".repeat(numArray[numArray.length - 4]);
+    }
 }
