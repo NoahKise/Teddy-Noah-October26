85346156de68fedfe957f96aa3c228ad2d80ba34
noahkise <noah@kisefamily.com>
2023-10-26 15:08:06 -0700

    crypto pass square root rounding test
    Co-authored-by Teddy Peterschmidt <t.a.peterschmidt@gmail.com>:

--- cryptosquare/js/scripts.js
+++ cryptosquare/js/scripts.js
@@ -2,14 +2,21 @@ function removingSpacesAndPunctuation(input) {
     const lettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     const editedArray = [];
     const inputArray = input.toLowerCase().split("")
-    // const inputArray = input.split("")
     inputArray.forEach(function(element) {
         if (lettersArray.includes(element)) {
             editedArray.push(element);
             console.log(editedArray);
         }
     })
-
-    
-
+    const squareRoot = Math.sqrt(editedArray.length);
+    const roundedRoot = Math.floor(squareRoot);
+    console.log(roundedRoot);
 }
+
+// function splitArrayBySquareRoot(input) {
+//     const squareRoot = Math.sqrt(removingSpacesAndPunctuation.length);
+//     const roundedRoot = Math.floor(squareRoot);
+//     console.log(roundedRoot);
+
+
+// }
